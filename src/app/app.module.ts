b58e7c67be49bcf63554431c036c8dbc2cca85a7
Godoi, Glauco <CSTGODOIG@CORP.JTI.COM>
2020-09-22 18:28:10 +0200

    Creating the landing page and the logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { MatButtonModule} from '@angular/material/button';
+//import {} from '@angular/material'
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { environment } from '../environments/environment';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
